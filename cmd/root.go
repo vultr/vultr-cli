826fde319af12e2d755d0e318307fe162ebb2774
David Dymko <dymkod@gmail.com>
2019-06-05 10:29:04 -0400

    network command

--- cmd/root.go
+++ cmd/root.go
@@ -69,6 +69,7 @@ func init() {
 	rootCmd.AddCommand(backupCmd)
 	rootCmd.AddCommand(BlockStorageCmd())
 	rootCmd.AddCommand(Dns())
+	rootCmd.AddCommand(Network())
 	rootCmd.AddCommand(osCmd)
 	rootCmd.AddCommand(Regions())
 	rootCmd.AddCommand(Script())
