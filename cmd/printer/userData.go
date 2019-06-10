29fce7235ee21f282527a28b66f10fb7b2da6b09
Fady Farid <ffarid@vultr.com>
2019-06-10 10:31:10 -0400

    add bare metal server commands

---
+++ cmd/printer/userData.go
@@ -0,0 +1,20 @@
+package printer
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+
+	"github.com/vultr/govultr"
+)
+
+func UserData(u *govultr.UserData) {
+	display(columns{"USERDATA"})
+	data, err := base64.StdEncoding.DecodeString(u.UserData)
+	if err != nil {
+		fmt.Printf("Error decoding user-data: %v\n", err)
+		os.Exit(1)
+	}
+	display(columns{string(data)})
+	flush()
+}
