ef9989f9e02d04c8005574decd4745859f360a03
Fady Farid <ffarid@vultr.com>
2019-06-12 09:38:06 -0400

    remove default description

--- cmd/root.go
+++ cmd/root.go
@@ -31,13 +31,8 @@ var client *govultr.Client
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "vultr-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "vultr-cli is a command line interface for the Vultr API",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
