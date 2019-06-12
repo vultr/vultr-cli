588f30ca9ea187806580190513f0f2f738da0679
David Dymko <dymkod@gmail.com>
2019-06-12 11:45:37 -0400

    makefile + readme + contributing

--- Makefile
+++ Makefile
@@ -3,45 +3,41 @@ export CGO=0
 
 DIR = $(shell echo $${DIR:-$(shell pwd)/builds})
 
-
-# Default values
-GOOS = linux
-GOARCH = amd64
-
-UNAME_S := $(shell uname -s)
-UNAME_P := $(shell uname -p)
-
-# If machine is a mac
-ifeq ($(UNAME_S),Darwin)
-  GOOS = darwin
-  GOARCH = 386
-endif
-
-# If machine is 32bit
-ifneq ($(UNAME_P), x86_64)
-  GOARCH = 386
-endif
-
-# Builds for current machine
-build: _build
-
 # Builds for mac os
 build_mac: GOOS = darwin
-build_mac: GOARCH = 386
+build_mac: GOARCH = amd64
 build_mac: _build
 
 # Builds for linux 32bit
+build_linux_386: GOOS = linux
 build_linux_386: GOARCH = 386
 build_linux_386: _build
 
 # Builds for linux 64bit
+build_linux_amd64: GOOS = linux
 build_linux_amd64: GOARCH = amd64
 build_linux_amd64: _build
+
+# Builds for Windows 64bit
+build_windows_64: GOOS = windows
+build_windows_64: GOARCH = amd64
+build_windows_64: _build_win
+
+# Builds for Windows 64bit
+build_windows_64: GOOS = windows
+build_windows_64: GOARCH = 386
+build_windows_64: _build_win
 
 _build:
 	@mkdir -p builds
 	@echo "building vultr-cli"
 	env GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o $(DIR)/vultr-cli_$(GOOS)_$(GOARCH)
+	@echo "built $(DIR)/vultr-cli_$(GOOS)_$(GOARCH)"
+
+_build_win:
+	@mkdir -p builds
+	@echo "building vultr-cli"
+	env GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o $(DIR)/vultr-cli_$(GOOS)_$(GOARCH).exe
 	@echo "built $(DIR)/vultr-cli_$(GOOS)_$(GOARCH)"
 
 remove:
--- cmd/root.go
+++ cmd/root.go
@@ -31,16 +31,8 @@ var client *govultr.Client
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "vultr-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "vultr-cli is a command line interface for the Vultr API",
+	Long: ``,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -55,14 +47,8 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	cobra.OnInitialize(initClient)
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.vultr-cli.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	rootCmd.AddCommand(accountCmd)
 	rootCmd.AddCommand(apiCmd)
 	rootCmd.AddCommand(applicationCmd)
