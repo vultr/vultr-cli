c5f94a1e0e786840e51f3d7067e5356e02ca543c
David Dymko <dymkod@gmail.com>
2020-10-17 17:53:59 -0400

    cleaned up printer columns

--- cmd/printer/os.go
+++ cmd/printer/os.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Os(vultrOS []govultr.OS, meta *govultr.Meta) {
-	col := columns{"OSID", "NAME", "ARCH", "FAMILY"}
+	col := columns{"ID", "NAME", "ARCH", "FAMILY"}
 	display(col)
 	for _, os := range vultrOS {
 		display(columns{os.ID, os.Name, os.Arch, os.Family})
--- cmd/printer/snapshot.go
+++ cmd/printer/snapshot.go
@@ -5,14 +5,14 @@ import (
 )
 
 func Snapshot(snapshot *govultr.Snapshot) {
-	display(columns{"SNAPSHOTID", "DATE CREATED", "SIZE", "STATUS", "OSID", "APPID", "DESCRIPTION"})
+	display(columns{"ID", "DATE CREATED", "SIZE", "STATUS", "OSID", "APPID", "DESCRIPTION"})
 	display(columns{snapshot.ID, snapshot.DateCreated, snapshot.Size, snapshot.Status, snapshot.OsID, snapshot.AppID, snapshot.Description})
 
 	flush()
 }
 
 func Snapshots(snapshot []govultr.Snapshot, meta *govultr.Meta) {
-	col := columns{"SNAPSHOTID", "DATE CREATED", "SIZE", "STATUS", "OSID", "APPID", "DESCRIPTION"}
+	col := columns{"ID", "DATE CREATED", "SIZE", "STATUS", "OSID", "APPID", "DESCRIPTION"}
 	display(col)
 	for _, s := range snapshot {
 		display(columns{s.ID, s.DateCreated, s.Size, s.Status, s.OsID, s.AppID, s.Description})
