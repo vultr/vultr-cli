c38465b1001a22f96f21f0a5ef91df1e4736cce0
Fady Farid <ffarid@vultr.com>
2022-02-04 13:49:32 -0500

    add ip type, source and subnet size to firewall rule printer

--- cmd/printer/firewallRule.go
+++ cmd/printer/firewallRule.go
@@ -1,14 +1,16 @@
 package printer
 
 import (
+	"fmt"
+
 	"github.com/vultr/govultr/v2"
 )
 
 func FirewallRules(fwr []govultr.FirewallRule, meta *govultr.Meta) {
-	col := columns{"RULE NUMBER", "ACTION", "PROTOCOL", "PORT", "NETWORK", "NOTES"}
+	col := columns{"RULE NUMBER", "ACTION", "TYPE", "PROTOCOL", "PORT", "NETWORK", "SOURCE", "NOTES"}
 	display(col)
 	for _, f := range fwr {
-		display(columns{f.ID, f.Action, f.Protocol, f.Port, f.Subnet, f.Notes})
+		display(columns{f.ID, f.Action, f.Type, f.Protocol, f.Port, getFirewallNetwork(f.Subnet, f.SubnetSize), getFirewallSource(f.Source), f.Notes})
 	}
 
 	Meta(meta)
@@ -16,9 +18,19 @@ func FirewallRules(fwr []govultr.FirewallRule, meta *govultr.Meta) {
 }
 
 func FirewallRule(fwr *govultr.FirewallRule) {
-	col := columns{"RULE NUMBER", "ACTION", "PROTOCOL", "PORT", "NETWORK", "NOTES"}
+	col := columns{"RULE NUMBER", "ACTION", "TYPE", "PROTOCOL", "PORT", "NETWORK", "SOURCE", "NOTES"}
 	display(col)
-
-	display(columns{fwr.ID, fwr.Action, fwr.Protocol, fwr.Port, fwr.Subnet, fwr.Notes})
+	display(columns{fwr.ID, fwr.Action, fwr.Type, fwr.Protocol, fwr.Port, getFirewallNetwork(fwr.Subnet, fwr.SubnetSize), getFirewallSource(fwr.Source), fwr.Notes})
 	flush()
+}
+
+func getFirewallSource(source string) string {
+	if source == "" {
+		return "anywhere"
+	}
+	return source
+}
+
+func getFirewallNetwork(subnet string, size int) string {
+	return fmt.Sprintf("%s/%d", subnet, size)
 }
