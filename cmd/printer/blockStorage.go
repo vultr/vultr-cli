c37704a4f518db335501602dd6415ec0d57628c8
David Dymko <dymkod@gmail.com>
2022-04-14 10:06:35 -0400

    adding block type to printer response

--- cmd/printer/blockStorage.go
+++ cmd/printer/blockStorage.go
@@ -7,11 +7,11 @@ import (
 )
 
 func BlockStorage(bs []govultr.BlockStorage, meta *govultr.Meta) {
-	col := columns{"ID", "REGION ID", "INSTANCE ID", "SIZE GB", "STATUS", "LABEL", "DATE CREATED", "MONTHLY COST", "MOUNT ID"}
+	col := columns{"ID", "REGION ID", "INSTANCE ID", "SIZE GB", "STATUS", "LABEL", "BLOCK TYPE", "DATE CREATED", "MONTHLY COST", "MOUNT ID"}
 	display(col)
 	for _, b := range bs {
 		cost := fmt.Sprintf("$%v", b.Cost)
-		display(columns{b.ID, b.Region, b.AttachedToInstance, b.SizeGB, b.Status, b.Label, b.DateCreated, cost, b.MountID})
+		display(columns{b.ID, b.Region, b.AttachedToInstance, b.SizeGB, b.Status, b.Label, b.BlockType, b.DateCreated, cost, b.MountID})
 	}
 
 	Meta(meta)
@@ -19,9 +19,9 @@ func BlockStorage(bs []govultr.BlockStorage, meta *govultr.Meta) {
 }
 
 func SingleBlockStorage(b *govultr.BlockStorage) {
-	col := columns{"ID", "REGION ID", "INSTANCE ID", "SIZE GB", "STATUS", "LABEL", "DATE CREATED", "MONTHLY COST", "MOUNT ID"}
+	col := columns{"ID", "REGION ID", "INSTANCE ID", "SIZE GB", "STATUS", "LABEL", "BLOCK TYPE", "DATE CREATED", "MONTHLY COST", "MOUNT ID"}
 	display(col)
 	cost := fmt.Sprintf("$%v", b.Cost)
-	display(columns{b.ID, b.Region, b.AttachedToInstance, b.SizeGB, b.Status, b.Label, b.DateCreated, cost, b.MountID})
+	display(columns{b.ID, b.Region, b.AttachedToInstance, b.SizeGB, b.Status, b.Label, b.BlockType, b.DateCreated, cost, b.MountID})
 	flush()
 }
