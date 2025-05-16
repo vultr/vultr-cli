2872df4eec9e12915c5ff954eef1918bbd2e09c2
Michael Riley <mriley@vultr.com>
2025-05-16 13:12:01 -0400

    Add account bandwidth command

--- cmd/account/printer.go
+++ cmd/account/printer.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/vultr/govultr/v3"
@@ -51,5 +52,107 @@ func (a *AccountPrinter) Data() [][]string {
 
 // Paging ...
 func (a *AccountPrinter) Paging() [][]string {
+	return nil
+}
+
+// ======================================
+
+// AccountPrinter ...
+type AccountBandwidthPrinter struct {
+	Bandwidth *govultr.AccountBandwidth `json:"account_bandwidth"`
+}
+
+// JSON ...
+func (a *AccountBandwidthPrinter) JSON() []byte {
+	return printer.MarshalObject(a, "json")
+}
+
+// YAML ...
+func (a *AccountBandwidthPrinter) YAML() []byte {
+	return printer.MarshalObject(a, "yaml")
+}
+
+// Columns ...
+func (a *AccountBandwidthPrinter) Columns() [][]string {
+	return [][]string{0: {
+		"PERIOD",
+		"GB IN",
+		"GB OUT",
+		"INSTANCE HOURS",
+		"INSTANCE COUNT",
+		"CREDITS INSTANCE",
+		"CREDITS FREE",
+		"CREDITS PURCHASED",
+		"OVERAGE",
+		"OVERAGE UNIT COST",
+		"OVERAGE COST",
+	}}
+}
+
+// Data ...
+func (a *AccountBandwidthPrinter) Data() [][]string {
+	previousPeriod := fmt.Sprintf(
+		"%s - %s",
+		utils.ParseAPITimestamp(a.Bandwidth.PreviousMonth.TimestampStart),
+		utils.ParseAPITimestamp(a.Bandwidth.PreviousMonth.TimestampEnd),
+	)
+
+	currentPeriod := fmt.Sprintf(
+		"%s - %s",
+		utils.ParseAPITimestamp(a.Bandwidth.CurrentMonthToDate.TimestampStart),
+		utils.ParseAPITimestamp(a.Bandwidth.CurrentMonthToDate.TimestampEnd),
+	)
+
+	projectedPeriod := fmt.Sprintf(
+		"%s - %s",
+		utils.ParseAPITimestamp(a.Bandwidth.CurrentMonthProjected.TimestampStart),
+		utils.ParseAPITimestamp(a.Bandwidth.CurrentMonthProjected.TimestampEnd),
+	)
+
+	return [][]string{
+		0: {
+			previousPeriod,
+			strconv.Itoa(a.Bandwidth.PreviousMonth.GBIn),
+			strconv.Itoa(a.Bandwidth.PreviousMonth.GBOut),
+			strconv.Itoa(a.Bandwidth.PreviousMonth.TotalInstanceHours),
+			strconv.Itoa(a.Bandwidth.PreviousMonth.TotalInstanceCount),
+			strconv.Itoa(a.Bandwidth.PreviousMonth.InstanceBandwidthCredits),
+			strconv.Itoa(a.Bandwidth.PreviousMonth.FreeBandwidthCredits),
+			strconv.Itoa(a.Bandwidth.PreviousMonth.PurchasedBandwidthCredits),
+			strconv.FormatFloat(float64(a.Bandwidth.PreviousMonth.Overage), 'f', utils.FloatPrecision, 32),
+			strconv.FormatFloat(float64(a.Bandwidth.PreviousMonth.OverageUnitCost), 'f', utils.FloatPrecision, 32),
+			strconv.FormatFloat(float64(a.Bandwidth.PreviousMonth.OverageCost), 'f', utils.FloatPrecision, 32),
+		},
+		1: {
+			currentPeriod,
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.GBIn),
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.GBOut),
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.TotalInstanceHours),
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.TotalInstanceCount),
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.InstanceBandwidthCredits),
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.FreeBandwidthCredits),
+			strconv.Itoa(a.Bandwidth.CurrentMonthToDate.PurchasedBandwidthCredits),
+			strconv.FormatFloat(float64(a.Bandwidth.CurrentMonthToDate.Overage), 'f', utils.FloatPrecision, 32),
+			strconv.FormatFloat(float64(a.Bandwidth.CurrentMonthToDate.OverageUnitCost), 'f', utils.FloatPrecision, 32),
+			strconv.FormatFloat(float64(a.Bandwidth.CurrentMonthToDate.OverageCost), 'f', utils.FloatPrecision, 32),
+		},
+		2: {
+			projectedPeriod,
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.GBIn),
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.GBOut),
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.TotalInstanceHours),
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.TotalInstanceCount),
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.InstanceBandwidthCredits),
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.FreeBandwidthCredits),
+			strconv.Itoa(a.Bandwidth.CurrentMonthProjected.PurchasedBandwidthCredits),
+			strconv.FormatFloat(float64(a.Bandwidth.CurrentMonthProjected.Overage), 'f', utils.FloatPrecision, 32),
+			strconv.FormatFloat(float64(a.Bandwidth.CurrentMonthProjected.OverageUnitCost), 'f', utils.FloatPrecision, 32),
+			strconv.FormatFloat(float64(a.Bandwidth.CurrentMonthProjected.OverageCost), 'f', utils.FloatPrecision, 32),
+		},
+	}
+}
+
+// Paging ...
+func (a *AccountBandwidthPrinter) Paging() [][]string {
 	return nil
 }
