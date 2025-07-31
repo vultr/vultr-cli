1136dec41b6c2de9b1c381ed035730f9d36cf8f2
Matthew Monaco <matt@monaco.cx>
2025-07-31 15:51:22 -0600

    snapshots: Add create-url --description flag

--- cmd/snapshot/snapshot.go
+++ cmd/snapshot/snapshot.go
@@ -126,6 +126,12 @@ func NewCmdSnapshot(base *cli.Base) *cobra.Command {
 				URL: url,
 			}
 
+			if desc, err := cmd.Flags().GetString("description"); err != nil {
+				return fmt.Errorf("error parsing flag 'description' for createURL : %v", err)
+			} else {
+				o.URLReq.Description = desc
+			}
+
 			if cmd.Flags().Changed("uefi") {
 				if b, err := cmd.Flags().GetBool("uefi"); err != nil {
 					return fmt.Errorf("error parsing flag 'uefi' for createURL : %v", err)
@@ -147,6 +153,7 @@ func NewCmdSnapshot(base *cli.Base) *cobra.Command {
 	}
 
 	createURL.Flags().StringP("url", "u", "", "Remote URL from where the snapshot will be downloaded.")
+	createURL.Flags().StringP("description", "d", "", "Set snapshot description (defaults to filename from request URL).")
 	createURL.Flags().Bool("uefi", false, "Mark snapshot as UEFI.")
 	if err := createURL.MarkFlagRequired("url"); err != nil {
 		fmt.Printf("error marking snapshot create 'url' flag required: %v", err)
