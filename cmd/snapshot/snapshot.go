cddc86dbd28876398149b7baa57d474296fd4e72
Matt Monaco <matt@monaco.cx>
2025-09-03 11:27:21 -0600

    Add UEFI and description flags to snapshot create-url (#547)
    * snapshots: Add create-url --uefi boolean flag
    
    Differentiate between unset and false by using flags.Changed().
    
    (Note the style in here leads to a lot of unecessary error checking --
    and hence reference to string flag names. IMO it's nicer to create a
    flags/vals struct and use the *Var* variants from pflags...)
    
    (This is on top of https://github.com/vultr/govultr/pull/390)
    
    * snapshots: Add create-url --description flag

--- cmd/snapshot/snapshot.go
+++ cmd/snapshot/snapshot.go
@@ -126,6 +126,20 @@ func NewCmdSnapshot(base *cli.Base) *cobra.Command {
 				URL: url,
 			}
 
+			if desc, err := cmd.Flags().GetString("description"); err != nil {
+				return fmt.Errorf("error parsing flag 'description' for createURL : %v", err)
+			} else {
+				o.URLReq.Description = desc
+			}
+
+			if cmd.Flags().Changed("uefi") {
+				if b, err := cmd.Flags().GetBool("uefi"); err != nil {
+					return fmt.Errorf("error parsing flag 'uefi' for createURL : %v", err)
+				} else {
+					o.URLReq.UEFI = &b
+				}
+			}
+
 			snapshot, err := o.createURL()
 			if err != nil {
 				return fmt.Errorf("error creating snapshot from URL : %v", err)
@@ -139,6 +153,8 @@ func NewCmdSnapshot(base *cli.Base) *cobra.Command {
 	}
 
 	createURL.Flags().StringP("url", "u", "", "Remote URL from where the snapshot will be downloaded.")
+	createURL.Flags().StringP("description", "d", "", "Set snapshot description (defaults to filename from request URL).")
+	createURL.Flags().Bool("uefi", false, "Mark snapshot as UEFI.")
 	if err := createURL.MarkFlagRequired("url"); err != nil {
 		fmt.Printf("error marking snapshot create 'url' flag required: %v", err)
 		os.Exit(1)
