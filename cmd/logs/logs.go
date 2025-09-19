88eb4ca002350c35f552e4d446e03e3e7aa548f0
Michael Riley <mriley@vultr.com>
2025-09-19 16:54:53 -0400

    Add shortened flags

--- cmd/logs/logs.go
+++ cmd/logs/logs.go
@@ -24,6 +24,10 @@ var (
 	# Full example
 	vultr-cli logs list --start '2025-08-26T00:00:00Z' --end '2025-09-13T00:30:00Z' \
 		--uuid '8b903420-b2e3-4e4f-9f88-19efb30e1237' --type 'instances'
+
+	# Shortened with aliased commands
+	vultr-cli logs list -s '2025-08-26T00:00:00Z' -e '2025-09-13T00:30:00Z' \
+		-u '8b903420-b2e3-4e4f-9f88-19efb30e1237' -t 'instances'
 	`
 )
 
@@ -99,21 +103,26 @@ func NewCmdLogs(base *cli.Base) *cobra.Command {
 			return nil
 		},
 	}
-	list.Flags().String("start", "", "timestamp for the start of the time period from which to return logs")
+	list.Flags().StringP("start", "s", "", `
+a UTC timestamp for the start of the time period from which to return logs (ex. 2025-06-26T00:00:00Z)
+`)
 	if err := list.MarkFlagRequired("start"); err != nil {
 		fmt.Printf("error marking logs list 'start' flag required: %v", err)
 		os.Exit(1)
 	}
 
-	list.Flags().String("end", "", "timestamp for the end of the time period from which to return logs")
+	list.Flags().StringP("end", "e", "", `
+a UTC timestamp for the end of the time period from which to return logs (ex. 2025-06-28T00:00:00Z). 
+Must be later than the start timestamp and may not exceed 30 days and 1 hour difference from start.
+`)
 	if err := list.MarkFlagRequired("end"); err != nil {
 		fmt.Printf("error marking logs list 'end' flag required: %v", err)
 		os.Exit(1)
 	}
 
-	list.Flags().String("level", "", "filter logs by a level (info, debug, warning, error, critical)")
-	list.Flags().String("type", "", "filter logs by a resource type")
-	list.Flags().String("uuid", "", "filter logs by a resource UUID")
+	list.Flags().StringP("level", "l", "", "filter logs by a level (info, debug, warning, error, critical)")
+	list.Flags().StringP("type", "t", "", "filter logs by a resource type")
+	list.Flags().StringP("uuid", "u", "", "filter logs by a resource UUID")
 
 	cmd.AddCommand(
 		list,
